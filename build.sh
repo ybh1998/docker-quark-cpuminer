b337dddd7013882da5b89cb5abadd5fe35bacd41
ybh1998 <ybh1998@163.com>
2016-05-22 11:25:50 +0800

    nineth commit

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 apt-get update
-apt-get -y install git libjansson-dev libcurl4-openssl-dev automake
+apt-get -y install git libjansson-dev libcurl4-openssl-dev automake build-essential
 cat /proc/cpuinfo
 git clone https://github.com/uncle-bob/quarkcoin-cpuminer.git
 cd quarkcoin-cpuminer
